a7f4f24dd75f7cd4e04e1f037da4e6e9392a2092
warleon <51892390+warleon@users.noreply.github.com>
2021-02-11 18:25:39 -0500

    done tables

--- DB/Tables.sql
+++ DB/Tables.sql
@@ -1,8 +1,12 @@
 CREATE TABLE user(
-    firstname   text,
-    lastname    text,
-    username    text,
-    password    text
+    firstname   text not NULL,
+    lastname    text not NULL,
+    username    text PRIMARY KEY,
+    password    text not NULL
+);
+
+CREATE TABLE speciality(
+    title    text PRIMARY KEY
 );
 
 CREATE TABLE r_user_speciality(
@@ -11,10 +15,48 @@ CREATE TABLE r_user_speciality(
     CONSTRAINT pk_rus PRIMARY KEY (firstname,title)
 );
 
-CREATE TABLE speciality(
-    title    text
+CREATE TABLE achivement(
+    title   text PRIMARY KEY,
+    document bytea not NULL,
 );
 
-CREATE TABLE achivement(
+CREATE TABLE r_user_achivemet(
+    firstname text FOREIGN KEY REFERENCES user(firstname),
+    title   text FOREIGN KEY REFERENCES achivement(title),
+    CONSTRAINT pk_rua PRIMARY KEY (firstname, title)
+);
 
+CREATE TABLE dpoint(
+    id int PRIMARY KEY,
+    fid int FOREIGN KEY REFERENCES dpoint(id),
+    statement text not NULL,
+    participation   int not NULL,
+    totalscore  int not NULL,
+    CONSTRAINT u_id_fid UNIQUE (id,fid),
+    CHECK   (id <> fid)
+);
+
+CREATE TABLE comment(
+    id  int PRIMARY KEY,
+    dpoint  int FOREIGN KEY REFERENCES dpoint(id),
+    content text not NULL,
+    likes   int not NULL,
+    dislikes    int not NULL
+);
+
+CREATE TABLE tag(
+    name    text PRIMARY KEY
+);
+
+CREATE TABLE author(
+    name    text PRIMARY KEY
+);
+
+CREATE TABLE paper(
+    title text PRIMARY KEY,
+    tags text FOREIGN KEY REFERENCES tag(name),
+    author text FOREIGN KEY REFERENCES author(name),
+    based_on text FOREIGN KEY REFERENCES paper(title),
+    pusblisher text,
+    pubd date
 );
