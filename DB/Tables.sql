588060a1102a83a0cbbc7146a2bf2879e3f45c89
warleon <51892390+warleon@users.noreply.github.com>
2021-02-11 19:34:52 -0500

    now it works i hope

--- DB/Tables.sql
+++ DB/Tables.sql
@@ -1,62 +1,80 @@
-CREATE TABLE user(
+drop table if exists my_user;
+CREATE TABLE my_user (
     firstname   text not NULL,
     lastname    text not NULL,
-    username    text PRIMARY KEY,
+    username    text  PRIMARY KEY,
     password    text not NULL
 );
 
+drop table if exists speciality;
 CREATE TABLE speciality(
     title    text PRIMARY KEY
 );
 
+drop table if exists r_user_speciality;
 CREATE TABLE r_user_speciality(
-    firstname text FOREIGN KEY REFERENCES user(firstname),
-    title text FOREIGN KEY REFERENCES user(firstname),
-    CONSTRAINT pk_rus PRIMARY KEY (firstname,title)
+    username text, 
+    title text,
+    CONSTRAINT pk_rus PRIMARY KEY (username,title),
+    CONSTRAINT fk_ufn FOREIGN KEY (username) REFERENCES my_user(username),
+    CONSTRAINT fk_st FOREIGN KEY (title) REFERENCES speciality(title)
 );
 
+drop table if exists achivement;
 CREATE TABLE achivement(
     title   text PRIMARY KEY,
-    document bytea not NULL,
+    document bytea not NULL
 );
 
+drop table if exists r_user_achivemet;
 CREATE TABLE r_user_achivemet(
-    firstname text FOREIGN KEY REFERENCES user(firstname),
-    title   text FOREIGN KEY REFERENCES achivement(title),
-    CONSTRAINT pk_rua PRIMARY KEY (firstname, title)
+    username text,
+    title   text,
+    CONSTRAINT pk_rua PRIMARY KEY (username, title),
+    CONSTRAINT fk_rufn FOREIGN KEY (username) REFERENCES my_user(username),
+    CONSTRAINT fk_rat FOREIGN KEY (title) REFERENCES achivement(title)
 );
 
+drop table if exists dpoint;
 CREATE TABLE dpoint(
     id int PRIMARY KEY,
-    fid int FOREIGN KEY REFERENCES dpoint(id),
-    statement text not NULL,
+    fid int,
+    thecontent text not NULL,
     participation   int not NULL,
     totalscore  int not NULL,
     CONSTRAINT u_id_fid UNIQUE (id,fid),
     CHECK   (id <> fid)
 );
 
+drop table if exists comment;
 CREATE TABLE comment(
     id  int PRIMARY KEY,
-    dpoint  int FOREIGN KEY REFERENCES dpoint(id),
-    content text not NULL,
+    dpoint  int,
+    thecontent text not NULL,
     likes   int not NULL,
-    dislikes    int not NULL
+    dislikes    int not NULL,
+    CONSTRAINT fk_cdpid FOREIGN KEY (dpoint) REFERENCES dpoint(id)
 );
 
+drop table if exists tag;
 CREATE TABLE tag(
     name    text PRIMARY KEY
 );
 
+drop table if exists author;
 CREATE TABLE author(
     name    text PRIMARY KEY
 );
 
+drop table if exists paper;
 CREATE TABLE paper(
     title text PRIMARY KEY,
-    tags text FOREIGN KEY REFERENCES tag(name),
-    author text FOREIGN KEY REFERENCES author(name),
-    based_on text FOREIGN KEY REFERENCES paper(title),
+    tag text,
+    author text,
+    based_on text,
     pusblisher text,
-    pubd date
+    pubd date,
+    CONSTRAINT fk_ptn FOREIGN KEY (tag) REFERENCES tag(name),
+    CONSTRAINT fk_pan FOREIGN KEY (author) REFERENCES author(name),
+    CONSTRAINT fk_ppt FOREIGN KEY (based_on) REFERENCES paper(title)
 );
