d14e8b9ca40e46bc9158ae3222de495e7358b288
warleon <51892390+warleon@users.noreply.github.com>
2021-02-12 22:06:39 -0500

    asdasd

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:postgres@localhost:5432/hack2021"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class my_user_model(db.Model):
+    __tablename__ = 'my_user'
+    firstname = db.Column(db.String())
+    lastname  = db.Column(db.String())
+    username  = db.Column(db.String(), primary_key=True)
+    password  = db.Column(db.String())
+
+    def __init__(self, firstname, lastname, username, password):
+        self.firstname=firstname
+        self.lastname=lastname
+        self.username=username
+        self.password=password
+
 
 @app.route('/')
 def index():
